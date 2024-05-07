004625954043eac22ea7660361972278867b8443
Dina Abd El-Halim <91435311+Dinahalem@users.noreply.github.com>
2024-05-07 08:41:54 +0300

    Update iam.tf

--- eks-setup/iam.tf
+++ eks-setup/iam.tf
@@ -1,23 +1,23 @@
 resource "aws_iam_role" "demo" {
-  name = "eks-cluster-demo"
-
-  assume_role_policy = <<Policy
+  name               = "demo"
+  assume_role_policy = <<POLICY
 {
-    "Version" : "2012-10-17"
-    "Statement" : [
-      {
-        #Action : "sts:AssumeRole"
-        "Effect" : "Allow"
-        "Principal" : {
-         "Service" : "eks.amazonaws.com"
+  "Version" : "2012-10-17",
+  "Statement" : [
+    {
+      "Effect" : "Allow",
+      "Principal" : {
+        "Service" : "eks.amazonaws.com"
       },
       "Action": "sts:AssumeRole"
-     }
-    ]
+    }
+  ]
 }
 POLICY
 }
+
 resource "aws_iam_role_policy_attachment" "demo-AmazonEKSClusterPolicy" {
- policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role      = aws_iam_role.demo.name
-} 
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.demo.name
+}
+
